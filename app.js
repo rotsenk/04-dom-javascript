4fbefe810c9ed5b7564e437141a6578d25b00ac1
rotsenk <stanley.95@hotmail.com>
2024-02-24 14:03:24 -0600

    leer-modificar

--- app.js
+++
@@ -1,8 +0,0 @@
-// document.querySelector('h3')
-// console.log(document.querySelector('h3'))
-// console.log(document.querySelector('.h3-danger'))
-// console.log(document.querySelector('#parrafo'))
-// console.log(document.getElementById('h3-secondary'))
-// console.log(document.querySelectorAll('.h3-danger'))
-// console.log(document.querySelectorAll('h3'))
-
